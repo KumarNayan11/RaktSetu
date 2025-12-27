f7d14697cdb4cd3d81d3e2ea5b9f81c30572b5a7
Nayan <199917572+KumarNayan11@users.noreply.github.com>
2025-12-27 18:06:18 +0530

    Updating src/app/page.tsx to solve deployment error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from 'react';
 import { PublicRequests } from '@/components/requests/PublicRequests';
 import { RaktSahayak } from '@/components/RaktSahayak';
 
 export default function Home() {
   return (
     <div className="w-full">
-      <PublicRequests />
-      <RaktSahayak />
+      <Suspense fallback={<div className="p-4 text-center">Loading...</div>}>
+        <PublicRequests />
+        <RaktSahayak />
+      </Suspense>
     </div>
   );
 }
